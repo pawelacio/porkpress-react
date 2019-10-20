b5d0485feceab77bbadac025ae24bad6e3301449
Paweł Kaczmarek <pawel.kaczmarek@crafton.pl>
2019-10-20 21:13:53 +0200

    Basic headings components

---
+++ components/styles/headers.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Head1 = styled.h1`
+  font-size: 42px;
+`;
+
+export const Head2 = styled.h2`
+  font-size: 32px;
+`;
+
+export const Head3 = styled.h3`
+  font-size: 24px;
+`;
+
+export const Head4 = styled.h4`
+  font-size: 16px;
+`;
+
+export const Head5 = styled.h5`
+  font-size: 12px;
+`;
+
+export const Head6 = styled.h6`
+  font-size: 10px;
+`;
