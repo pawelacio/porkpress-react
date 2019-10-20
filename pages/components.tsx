187d3b89777f2949ec1349b96da085e83350b39e
Paweł Kaczmarek <pawel.kaczmarek@crafton.pl>
2019-10-20 21:42:47 +0200

    Components page update

--- pages/components.tsx
+++ pages/components.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { Layout } from '../components/layout/layout.component';
 import { GridContainer, GridElement, GridSampler, GridSpacer } from '../components/layout/grid/grid.styled';
 import { Input } from '../components/ui/inputs/input/input.component';
+import { Head1, Head2, Head3, Head4, Head5, Head6 } from '../components/styles/headers';
 
 const Index = () => (
   <Layout>
     <GridSpacer/>
+    <GridContainer>
+      <GridElement>
+        <Head1>Grid</Head1>
+      </GridElement>
+    </GridContainer>
     <GridContainer>
       <GridElement width={ 12 }><GridSampler>col-12</GridSampler></GridElement>
     </GridContainer>
@@ -48,11 +54,37 @@ const Index = () => (
     </GridContainer>
     <GridSpacer/>
     <GridContainer>
+      <GridElement>
+        <Head1>Typography exaples</Head1>
+      </GridElement>
+      <GridElement>
+        <Head1>This is heading h1</Head1>
+      </GridElement>
+      <GridElement>
+        <Head2>This is heading h2</Head2>
+      </GridElement>
+      <GridElement>
+        <Head3>This is heading h3</Head3>
+      </GridElement>
+      <GridElement>
+        <Head4>This is heading h4</Head4>
+      </GridElement>
+      <GridElement>
+        <Head5>This is heading h5</Head5>
+      </GridElement>
+      <GridElement>
+        <Head6>This is heading h6</Head6>
+      </GridElement>
+    </GridContainer>
+    <GridContainer>
+      <GridElement>
+        <Head1>Inputs</Head1>
+      </GridElement>
       <GridElement width={ 3 }>
         <Input
           value=""
           type="text"
-          placeholder="Saample input"
+          placeholder="Sample input"
         />
       </GridElement>
       <GridElement width={ 3 }>
